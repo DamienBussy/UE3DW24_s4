d382f3cb59ef8a61a23d6b916ccb3d9eac92a7c2
Damien Bussy <damien.bussy@etu.unilim.fr>
2022-01-09 01:29:58 +0000

    partie 1

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { Users } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from "bcrypt";
+import { bcryptSaltRounds } from 'src/auth/constants';
 
 @Injectable()
 export class UsersService {
@@ -12,9 +14,26 @@ export class UsersService {
     private usersRepository: Repository<Users>
 ) { }
 
-  createUser(createUserDto: CreateUserDto) {
-    return this.usersRepository.save(createUserDto);
+  async createUser(createUserDto: CreateUserDto) {
+    const hashpassword = await this.encrypt(createUserDto.password)
+    return this.usersRepository.save(
+      {
+        firstname: createUserDto.firstname,
+        lastname: createUserDto.lastname,
+        email: createUserDto.email,
+        password: hashpassword
+      }
+    );
   }
+
+  async encrypt(password:string): Promise<any> {
+    return await bcrypt.hash(password, bcryptSaltRounds);
+  }
+
+  async hashCompare(reqPass, dbPass){
+    return bcrypt.compare(reqPass, dbPass);
+  }
+
 
   findAll() {
     return this.usersRepository.find();
@@ -22,6 +41,10 @@ export class UsersService {
 
   async findOne(id: number) {
     await this.usersRepository.findOne(id);
+  }
+
+  async findUser(query:any): Promise<Users> {
+    return await this.usersRepository.findOne(query);
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
